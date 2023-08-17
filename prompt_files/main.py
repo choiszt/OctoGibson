3664f87b7abf0b61f4811ad661a531b6b67abf00
dongyh20 <1342228579@qq.com>
2023-08-17 14:22:36 +0800

    update and rearrange files

---
+++ prompt_files/main.py
@@ -0,0 +1,12 @@
+import json
+from excecute_env import exec
+
+openai_api_key = "YOUR_API_KEY"
+
+with open("task_scene.json", 'r') as f:
+    data = json.load(f)
+
+for k, v in data.items():
+    exec(task_name=k, scene_name=v, json_path="./information.json", save_path='./action_data',
+         action_path='./action.py', openai_api_key='abcd')
+    
