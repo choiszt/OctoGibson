7f8d5e2e86e01abf902bba9ffcf2e821bbd273d7
Yuhao Dong <122193775+dongyh20@users.noreply.github.com>
2023-09-02 19:40:50 +0800

    update

--- prompt_files/gpt_request.py
+++ prompt_files/gpt_request.py
@@ -1,43 +1,46 @@
 import requests
 import json
 
-# Endpoint URL
-url = 'https://2077ai.com/ai-hub-api/app/chat-gpt'
 
-# Headers
-headers = {
-    'Content-Type': 'application/json'
-}
+def gpt_request(content=None):
+    # Endpoint URL
+    url = 'https://2077ai.com/ai-hub-api/app/chat-gpt'
 
-# Data payload
-data = {
-    "content": [
-        {
-            "role": "user",
-            "content": "Hello! I am Jack."
-        },
-        {
-            "role": "assistant",
-            "content": "Hello Jack! I am XiaoDuXiaoDu. How can I assist you today?"
-        },
-        {
-            "role": "user",
-            "content": "How to spell you name?"
-        }
-    ],
-    "model": "CHAT_GPT_4_32K",
-    "token": "sk-73dedelSgNkMqhiOaS63pie8hnTqt2LkICsN3VrwNCjPID0A"
-}
+    # Headers
+    headers = {
+        'Content-Type': 'application/json'
+    }
 
-# Make the POST request
-response = requests.post(url, headers=headers, json=data)
+    # Data payload
+    data = {
+        "content": [
+            {
+                "role": "user",
+                "content": content
+            }
+        ],
+        "model": "CHAT_GPT_4_32K",
+        "token": "sk-73dedelSgNkMqhiOaS63pie8hnTqt2LkICsN3VrwNCjPID0A"
+    }
 
-response = response.json()
+    # Make the POST request
+    response = requests.post(url, headers=headers, json=data)
 
-### this is what we need
-content = response["data"]["0"]["message"]["content"]
-# Check the response
-if response.status_code == 200:
-    print("Success:", json.dumps(response.json(), indent=4))
-else:
-    print("Failed:", response.content)
+    response = response.json()
+    
+    print(response)
+    
+    # import pdb;pdb.set_trace()
+
+    ### this is what we need
+    repsonse_content = response["data"]["0"]["message"]["content"]
+    print(repsonse_content)
+    # Check the response
+    if response["code"] == 200:
+        print("Success")
+    else:
+        print("Failed", response.content)
+    
+    return repsonse_content
+
+gpt_request("Hello, I'm dongyuhao")
