d0643eed9779027070e63663b66680402d068a89
Yuhao Dong <122193775+dongyh20@users.noreply.github.com>
2023-09-06 15:50:02 +0800

    update excel parse

--- prompt_files/parse_annotation.py
+++ prompt_files/parse_annotation.py
@@ -1,19 +1,30 @@
 import json
 
-def parse():
-    with open('./test.json') as f:
+def parse(num):
+    with open('./test.json', 'r') as f:
         data = json.load(f)
-        
-    original_task = data['task_name']
-    gpt_task = data['gpt_task']
-    remove_item = data['removed_ietm']
-    target_states = data['target_states']
-    obj_2 = []
-    obj_3 = []
-    for i in range(len(target_states)):
-        if len(target_states[i]) == 3:
-            obj_2.append(target_states[i])
-        if len(target_states[i]) == 4:
-            obj_3.append(target_states[i])
-    return original_task, gpt_task, remove_item, target_states, obj_2, obj_3
-
+    
+    for k, v in enumerate(data):
+        if k != num:
+            continue
+        task_data = data[v]
+        bddl_task = task_data['task_name']
+        gpt_task = task_data['gpt_task']
+        scene_name = task_data['env']
+        removed_items = task_data['removed_item']
+        target_states = task_data['target_states']
+        obj_2 = []
+        obj_3 = []
+        for i in range(len(target_states)):
+            if len(target_states[i]) == 3:
+                obj_2.append(target_states[i])
+            if len(target_states[i]) == 4:
+                obj_3.append(target_states[i])
+        parsed_data = {}
+        parsed_data['bddl'] = bddl_task
+        parsed_data['gpt'] = gpt_task
+        parsed_data['scene'] = scene_name
+        parsed_data['removed'] = removed_items
+        parsed_data['obj_2'] = obj_2
+        parsed_data['obj_3'] = obj_3
+    return parsed_data
--- prompt_files/parse_excel.py
+++ prompt_files/parse_excel.py
@@ -1,38 +1,44 @@
 import pandas as pd
 import json
 
-data = pd.read_excel(io='./BEHAVIOR_TASKS_ICLR.xlsx')
-task_name = data['TASK NAME']
-gpt_task = data['GPT TASK NAME']
+data = pd.read_excel(io='./tasks.xlsx', sheet_name='I1.cooking')
+print(data.keys())
+task_name = data['BDDL_Task']
+gpt_task = data['EVLM_Task']
 target_states = data['Target States']
-removed_item = data['Removed Item']
-print(task_name)
+removed_item = data['Removed Items']
+scene_name = data['Environment']
 all_tasks = {}
 for i in range(len(task_name)):
-     one_task = {}
-     one_task['task_name'] = task_name[i]
-     one_task['gpt_task'] = gpt_task[i]
-     r = removed_item[i]
-     r = r.split(',')
-     for j in range(len(r)):
-          r[j] = r[j].replace('{', '')
-          r[j] = r[j].replace(' ', '')
-          r[j] = r[j].replace('\'', '')
-          r[j] = r[j].replace('}', '')
-     one_task['removed_item'] = r
-     all_t = []
-     t = target_states[i]
-     t = t.split('\n')
-     num_t = len(t)
-     for ts in range(num_t):
-          t_s = t[ts].split(',')
-          for k in range(len(t_s)):
-               t_s[k] = t_s[k].replace(' ','')
-               t_s[k] = t_s[k].replace('[','')
-               t_s[k] = t_s[k].replace(']','')
-          all_t.append(t_s)
-     one_task['target_states'] = all_t
-     all_tasks[task_name[i]] = one_task
+    if gpt_task[i] == 'NA':
+        continue
+    try:
+        one_task = {}
+        one_task['task_name'] = task_name[i]
+        one_task['gpt_task'] = gpt_task[i]
+        r = removed_item[i]
+        r = r.split(',')
+        for j in range(len(r)):
+            r[j] = r[j].replace('[', '')
+            r[j] = r[j].replace(' ', '')
+            r[j] = r[j].replace(']', '')
+        one_task['removed_item'] = r
+        all_t = []
+        t = target_states[i]
+        t = t.split('\n')
+        num_t = len(t)
+        for ts in range(num_t):
+            t_s = t[ts].split(',')
+            for k in range(len(t_s)):
+                t_s[k] = t_s[k].replace(' ','')
+                t_s[k] = t_s[k].replace('[','')
+                t_s[k] = t_s[k].replace(']','')
+            all_t.append(t_s)
+        one_task['target_states'] = all_t
+        one_task['env'] = scene_name[i]
+        all_tasks[gpt_task[i]] = one_task
+    except: 
+        pass
      
 with open('test.json', 'w') as f:
      f.write(json.dumps(all_tasks))
