d96759da4e555175fe29858d113168d281f5462d
Yuhao Dong <122193775+dongyh20@users.noreply.github.com>
2023-09-07 19:06:03 +0800

    single for choiszt

--- prompt_files/execute_gpt_choiszt.py
+++ prompt_files/execute_gpt_choiszt.py
@@ -6,20 +6,24 @@ import yaml
 
 # from robot_action import *
 import parse_json
-import query
+import query_new as query
 from imp import reload
 import env_utils_gpt as eu 
 import openai
-import re
-import time
+# from gpt_request import gpt_request
+from gpt_request_azure import gpt_request
+
 def gpt_process(save_path, openai_api_key):
     # main task loop
     
     main_task_flag = False
     subtask_iter = 1
     gpt_query = query.Query(openai_api_key=openai_api_key)
+    for i in range(1, 15):
+        eu.f_mkdir(os.path.join(save_path, f"subtask_{i}"))
+
     while True:
-        print(f"create subtask_{subtask_iter}")
+
         # make the directory
         sub_save_path = eu.f_mkdir(os.path.join(save_path, f"subtask_{subtask_iter}"))
         
@@ -31,24 +35,28 @@ def gpt_process(save_path, openai_api_key):
             statinfo = os.stat(os.path.join(sub_save_path, 'task1.json')) 
             if statinfo.st_size > 0:
                 break
+        human_info = parse_json.parse_json(path=os.path.join(sub_save_path, "task1.json"))
         
         
         # subtask loop, when a subtask is finished, close the loop
         while True:
-            response_info = os.stat("/shared/liushuai/OmniGibson/prompt_files/gpt_response_choiszt.json") 
-            if response_info.st_size > 0:
-                break
-        while True:
-            with open("/shared/liushuai/OmniGibson/prompt_files/gpt_response_choiszt.json","r")as f:
-                response=json.load(f)
-            
+            system_message = gpt_query.render_system_message()
+            human_message = gpt_query.render_human_message(
+                scene_graph=human_info[0], object=human_info[1],
+                inventory=human_info[2], task=human_info[3] 
+            )
+            all_messages = [system_message, human_message]
+            content=system_message.content+"\n\n"+human_message.content
+            eu.save_input(sub_save_path, human_message.content)
+            print("start query")
+            response=gpt_request(content)
             try:
-                answer=response
+                answer = gpt_query.process_ai_message(response)
             except Exception as e:
                 answer = str(e)
                 print(answer)
             eu.save_response(sub_save_path, answer)
-            time.sleep(2)
+            
             while True:
                 if os.path.exists(os.path.join(sub_save_path, 'feedback.json')):
                     break
@@ -72,12 +80,18 @@ def gpt_process(save_path, openai_api_key):
                 else:
                     gpt_query.record_history(subtask=answer['subtask'], code=answer['code'], error=data['error'])
                     break
+        
 
+        
         # reset parameters
         subtask_iter += 1
         
+        if subtask_iter>15:
+            print(f"already attempt {subtask_iter} time, it is too long!")
+            break
+                
         if main_task_flag:
             break
 
 api_key="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"
-gpt_process(save_path="/shared/liushuai/OmniGibson/prompt_files/data",openai_api_key=api_key)
+gpt_process(save_path="/shared/liushuai/OmniGibson/prompt_files/data/cook_hot_dogs",openai_api_key=api_key)
--- prompt_files/execute_sim_choiszt.py
+++ prompt_files/execute_sim_choiszt.py
@@ -5,23 +5,24 @@ import yaml
 import omnigibson as og
 from omnigibson.macros import gm
 from omnigibson.utils.ui_utils import choose_from_options
-import sys
+
 from robot_action import *
 # import prompt_files.action as action
 from importlib import reload
+import importlib
 import env_utils_sim as eu 
 from initial_pipeline import *
 import time
 from bddl_verification import *
+import sys
+import bddl
+from verify_taskgoal import verify_taskgoal
 def sim_process(task_name, scene_name, action_path, save_path):
-
-    subtask_iter = 2
-
     heading="import os \nimport json\nimport yaml\nimport omnigibson as og\nfrom action_list import * \nfrom action_utils import *\n"
     config_filename="./prompt_files/bddl_task.yaml"
     cfg = yaml.load(open(config_filename, "r"), Loader=yaml.FullLoader)
     cfg["task"]["online_object_sampling"] = False
-    cfg["scene"]["scene_mdoel"] = scene_name
+    cfg["scene"]["scene_model"] = scene_name
     cfg["task"]["activity_name"] = task_name
     # Load the environment
     env = og.Environment(configs=cfg, action_timestep=1/60., physics_timestep=1/60.)
@@ -34,98 +35,157 @@ def sim_process(task_name, scene_name, action_path, save_path):
     camera.focal_length = 10.
     
     # main task loop
-    
-    main_succeed = False
-    
-    while True:
-        if os.path.exists(os.path.join(save_path, f"subtask_{subtask_iter}")):
-            break
-    sub_save_path = os.path.join(save_path, f"subtask_{subtask_iter}")
-    init_pipeline(env, robot, camera,task_name=str(task_name), file_name=sub_save_path)
-    response_path = os.path.join(sub_save_path, 'response.json')
-    feedback_path = os.path.join(sub_save_path, 'feedback.json')
-    
-    #subtask loop
-    while True:  
-        error=''
-        # wait for the GPT response
+    subtask_iter = 1
+    while True:     
+
+        main_succeed = False
+        
         while True:
-            if os.path.exists(response_path):
+            if os.path.exists(os.path.join(save_path, f"subtask_{subtask_iter}")):
                 break
-        while True:
-            response_info = os.stat(response_path) 
-            if response_info.st_size > 0:
-                break
-        with open(response_path) as f:
-            data = json.load(f)
-        answer = data['response']
-        if isinstance(answer, str):
-            subtask = ""
-            code = ""
-            error = answer
-            critic = 'fail'
+        sub_save_path = os.path.join(save_path, f"subtask_{subtask_iter}")
+        with open ("/shared/liushuai/OmniGibson/prompt_files/excel.json","r")as f:
+            a=json.load(f)
+            gpt_task_name=a[task_name]['gpt_task']
+        init_pipeline(env, robot, camera,task_name=str(gpt_task_name), file_name=sub_save_path)
+        response_path = os.path.join(sub_save_path, 'response.json')
+        feedback_path = os.path.join(sub_save_path, 'feedback.json')
+        
+        #subtask loop
+        while True:  
             reset = False
-            break
-        else:
-            subtask, code = answer['subtask'], answer['code']
-            with open(f"/shared/liushuai/OmniGibson/prompt_files/data/subtask_{subtask_iter}/action.py", 'w') as f:
-                f.write(heading)
-                f.write(code)
-            # time.sleep(2)
-            # if(subtask_iter!=1):
-            #     sys.path.remove(f"/shared/liushuai/OmniGibson/prompt_files/data/subtask_{subtask_iter-1}")
-            sys.path.append(f"/shared/liushuai/OmniGibson/prompt_files/data/subtask_{subtask_iter}")
-            import action
-            time.sleep(1)
-            try:
-                action.act(robot,env,camera)
-                print("act...")
-            except Exception as e:
-                error = str(e)
-                env.reset()
+            error=''
+            # wait for the GPT response
+            while True:
+                if os.path.exists(response_path):
+                    break
+            while True:
+                response_info = os.stat(response_path) 
+                if response_info.st_size > 0:
+                    break
+            with open(response_path) as f:
+                data = json.load(f)
+            answer = data['response']
+            if isinstance(answer, str):
+                subtask = ""
+                code = ""
+                error = answer
+                critic = 'fail'
+                reset = False
+                break
+            else:
+                subtask, code = answer['subtask'], answer['code']
+                with open(f"/shared/liushuai/OmniGibson/prompt_files/data/{task_name}/subtask_{subtask_iter}/action.py", 'w') as f:
+                    f.write(heading)
+                    f.write(code)
+                # time.sleep(2)
+                sys.path=list(set(sys.path))
+                if(subtask_iter!=1):
+                    sys.path.remove(f"/shared/liushuai/OmniGibson/prompt_files/data/{task_name}/subtask_{subtask_iter-1}")
+                sys.path.append(f"/shared/liushuai/OmniGibson/prompt_files/data/{task_name}/subtask_{subtask_iter}")
+                import action
+                time.sleep(1)
+                try:
+                    reload(action)
+                    time.sleep(2)
+                    action.act(robot,env,camera)
+                    print("act...")
+                except Exception as e:
+                    error = str(e)
+                    env.reset()
+                    robot.inventory=[]
+                    robot.visible_only=True
+                    subtask = subtask
+                    code = code
+                    error = error
+                    critic = 'fail'
+                    reset = True
+                    break
+            
+            # subtask verification
+            target_states = answer
+            # verify function
+            #comment the verification of inventory
+            # if target_states['inventory'] != 'None': #TODO string None
+            #     value = eu.verify_inv(env, robot, target_states['inventory'])
+            #     print(f"target_inv:{target_states['inventory']}")
+            #     print(f"inventory:{robot.inventory}")
+            #     if not value: #TODO need to fix the bug
+            #         error += f"{target_states['inventory']} is not in Inventory.\n"
+            for obj in target_states['obj_2']:
+                value = eu.verify_obj_2(env,obj[0], obj[1], obj[2])
+                if not value:
+                    error += f"State {obj[1]} of object {obj[0]} is not {obj[2]}\n"
+            # for obj in target_states['obj_3']:
+            #     value = eu.verify_obj_3(env,obj[0], obj[1], obj[2],obj[3])
+            #     if not value:
+            #         error += f"{obj[0]} is not {obj[1]} {obj[2]}\n"
+            if len(error) == 0:
                 subtask = subtask
-                code = code
+                error = error
+                critic = 'succeed'
+                reset = False
+                break
+            else:
+                subtask = subtask
                 error = error
                 critic = 'fail'
                 reset = True
+                env.reset()
+                robot.inventory=[]
                 break
-        print("start verify!")
-        # subtask verification
-        target_states = answer['target']
-        # verify function
-        if target_states['inv'] != 'None': #TODO string None
-            value = eu.verify_inv(env, robot, target_states['inv'])
-            if not value:
-                error += f"{target_states['inv']} is not in Inventory.\n"
-        for obj in target_states['obj_2']:
-            value = eu.verify_obj_2(env,obj[0], obj[1], obj[2])
-            if not value:
-                error += f"State {obj[1]} of object {obj[0]} is not {obj[2]}\n"
 
-        if len(error) == 0:
-            subtask = subtask
-            error = error
-            critic = 'succeed'
-            reset = False
-            break
-        else:
-            subtask = subtask
-            error = error
-            critic = 'fail'
-            reset = True
-            env.reset()
-            break
-    if critic == 'succeed':
-    # signal = verify_bddl()
-        signal=False
-        if signal:
-            main_succeed = True
-            eu.save_feedback(feedback_path, subtask, code, error, critic, reset, main_succeed)
-        else:
-            eu.save_feedback(feedback_path, subtask, code, error, critic, reset, main_succeed)
-    else:
-        eu.save_feedback(feedback_path, subtask, code, error, critic, reset, main_succeed)
-    print("finish!!")
-    time.sleep(3)
+        # reset parameters
 
-sim_process(task_name="cook_bacon",scene_name="Merom_1_int",action_path="./prompt_files/action.py",save_path="./prompt_files/data")
+        ###reset and run the previous code #TODO: NEED TO DELETE subtask_iter python.py *** CHOISZT 8.28
+        
+        # for iter_num in range(1,subtask_iter):
+        #     path=f"/shared/liushuai/OmniGibson/prompt_files/data/{task_name}/subtask_{iter_num}"
+        #     with open(os.path.join(path,"feedback.json"))as f:
+        #         tmp_feedback=json.load(f)
+        #     if tmp_feedback['critic']=='succeed':
+        #         sys.path.append(path)
+        #         import action
+        #         action.act(robot,env,camera)
+
+        subtask_iter += 1
+        #TODO choiszt need to add rename
+        #verify the whole task
+        if critic == 'succeed':
+            with open("/shared/liushuai/OmniGibson/prompt_files/excel.json","r")as f:
+                goal=json.load(f)
+            signal=False
+            target=goal[task_name]['target_states']
+            for tar in target:
+                if not verify_taskgoal(env,*tar):
+                    signal=False
+                    break
+                signal=True
+            if signal:
+                main_succeed = True
+                print(f"finish {task_name}!!!!! congrats!!!!!")
+                eu.save_feedback(feedback_path, subtask, code, error, critic, reset, main_succeed)
+                break
+            else:
+                eu.save_feedback(feedback_path, subtask, code, error, critic, reset, main_succeed)
+        else:
+            eu.save_feedback(feedback_path, subtask, code, error, critic, reset, main_succeed)
+        
+        if reset: #
+            if subtask_iter!=1:
+                for iter_num in range(1,subtask_iter):
+                    path=f"./prompt_files/data/{task_name}/subtask_{iter_num}"
+                    with open(os.path.join(path,"feedback.json"))as f:
+                        tmp_feedback=json.load(f)
+                    if tmp_feedback['critic']=='succeed':
+                        time.sleep(1)
+                        module=importlib.import_module(f"prompt_files.data.{task_name}.subtask_{iter_num}.action")
+                        print(f"prompt_files.data.{task_name}.subtask_{iter_num}.action retrieve")
+                        module.act(robot,env,camera)   
+        if subtask_iter>15:
+            print(f"already attempt {subtask_iter} time, it is too long!")
+            break
+                        
+
+
+sim_process(task_name="cook_hot_dogs",scene_name="Benevolence_1_int",action_path="./prompt_files/action.py",save_path=f"./prompt_files/data/cook_hot_dogs")
