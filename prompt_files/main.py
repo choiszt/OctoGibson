72acb28eec675e28135942996a6f9b86118b2a8f
Yuhao Dong <122193775+dongyh20@users.noreply.github.com>
2023-09-09 16:35:34 +0800

    update command

--- prompt_files/main.py
+++ prompt_files/main.py
@@ -1,35 +1,43 @@
 import os
 import json
-from parse_annotation import *
+from test_json import *
 import env_utils_sim as eu 
 from run_command import *
-import time
+
 TASK_NUM = 100
-base_path = './prompt_files/data'
-for i in range(10000):
+base_path = './data'
+for i in range(TASK_NUM):
     data = parse(i)
     task_name = data['bddl']
-    if i==131:
-        scene_name = data['scene']
-        gpt_name = data['gpt']
-        removed_item = data['removed']
-        target_states = data['target_states']
-        
-        save_path = eu.f_mkdir(os.path.join(base_path, gpt_name))
-        action_path = os.path.join(save_path, 'action.py')
-        
-        ### run command
-        command_args1 = {}
-        command_args2 = {}
-        command_args1['t'] = task_name
-        command_args1['s'] = scene_name
-        command_args1['g'] = gpt_name
-        command_args1['a'] = action_path
-        command_args1['r'] = removed_item
-        command_args1['target'] = target_states
-        command_args1['save'] = save_path
-        command_args2['save'] = save_path
-        
-        run_command_in_terminal(command_args2, process_name='gpt', conda_env='gpt')
-        run_command_in_terminal(command_args1, process_name='sim',conda_env='omni')
-        break
+    scene_name = data['scene']
+    gpt_name = data['gpt']
+    removed_item = data['removed']
+    target_states = data['target_states']
+    
+    save_path = eu.f_mkdir(os.path.join(base_path, gpt_name))
+    action_path = os.path.join(save_path, 'action.py')
+    
+    ### run command
+    command_args1 = {}
+    command_args2 = {}
+    command_args1['t'] = task_name
+    command_args1['s'] = scene_name
+    command_args1['g'] = gpt_name
+    command_args1['a'] = action_path
+    command_args1['r'] = removed_item
+    command_args1['target'] = target_states
+    command_args1['save'] = save_path
+    command_args2['save'] = save_path
+    
+    test1_command = "python prompt_files/execute_gpt.py"
+    test2_command = "python prompt_files/execute_sim.py --/log/level=error --/log/fileLogLevel=error --/log/outputStreamLevel=error"
+    conda_env1 = "gpt"
+    conda_env2 = "omni"
+    processes = []
+    processes.append(run_command_in_terminal(test1_command, conda_env1))
+    processes.append(run_command_in_terminal(test2_command, conda_env2))
+    
+    # run_command_in_terminal(command_args2, process_name='gpt', conda_env='gpt')
+    # run_command_in_terminal(command_args1, process_name='sim' conda_env='omni')
+    
+    check_processes(processes)
--- prompt_files/run_command.py
+++ prompt_files/run_command.py
@@ -1,20 +1,44 @@
 import subprocess
+import time
+import signal
+import os
 
-def run_command_in_terminal(command_args, process_name, conda_env):
+
+def run_command_in_terminal(command, conda_env):
     activate_env_command = f"source /opt/anaconda3/bin/activate {conda_env}"
-    if process_name == 'gpt':
-        command = f"python prompt_files/execute_gpt.py -save {command_args['save']}"
-    elif process_name == 'sim':
-        command = f"python prompt_files/execute_sim.py -t {command_args['t']} -g {command_args['g']} -s {command_args['s']} -a {command_args['a']} -save {command_args['save']} -r {command_args['r']} -target {command_args['target']} --/log/level=error --/log/fileLogLevel=error --/log/outputStreamLevel=error >>./choiszt_log.txt"
     full_command = f"bash -c '{activate_env_command} && {command}'"
-    subprocess.Popen(["gnome-terminal", "--", "bash", "-c", full_command])
+    return subprocess.Popen(["gnome-terminal", "--", "bash", "-c", full_command])
+
+
+def check_processes(processes):
+    while True:
+        for p in processes:
+            retcode = p.poll()
+            # 如果其中一个进程结束或异常退出
+            if retcode is not None:
+                terminate_processes(processes)
+                return
+
+        time.sleep(5)  # 每隔5秒检查一次进程的状态
+
+
+def terminate_processes(processes):
+    for p in processes:
+        if p.poll() is None:  # 如果进程仍然在运行
+            os.killpg(os.getpgid(p.pid), signal.SIGTERM)  # 杀死该进程及其子进程
+
 
 if __name__ == "__main__":
-    pass
-    # test1_command = "python prompt_files/execute_gpt.py"
-    # test2_command = "python prompt_files/execute_sim.py --/log/level=error --/log/fileLogLevel=error --/log/outputStreamLevel=error"
-    # conda_env1 = "gpt"
-    # conda_env2 = "omni"
+    test1_command = "python prompt_files/execute_gpt.py"
+    test2_command = "python prompt_files/execute_sim.py --/log/level=error --/log/fileLogLevel=error --/log/outputStreamLevel=error"
+    conda_env1 = "gpt"
+    conda_env2 = "omni"
 
-    # run_command_in_terminal(test1_command, conda_env1)
-    # run_command_in_terminal(test2_command, conda_env2)
+    processes = []
+    processes.append(run_command_in_terminal(test1_command, conda_env1))
+    processes.append(run_command_in_terminal(test2_command, conda_env2))
+
+    try:
+        check_processes(processes)
+    except KeyboardInterrupt:
+        terminate_processes(processes)
