3ea3dc1d98337ebf2b199a9af808142cdb297baa
Yuhao Dong <122193775+dongyh20@users.noreply.github.com>
2023-09-22 17:23:48 +0800

    update match

--- evaluation/match.py
+++ evaluation/match.py
@@ -36,6 +36,7 @@ def embed(words):
     
 def match(otter_word, sim_word):
     otter_emb = embed(otter_word)
+    print(otter_emb.shape)
     sim_emb = embed(sim_word)
     cos_sim = nn.CosineSimilarity(dim=1)
     match_words = []
@@ -45,10 +46,44 @@ def match(otter_word, sim_word):
         match_word = sim_word[torch.argmax(sim).item()]
         match_words.append(match_word)
     return match_words
+
+def change_code(code, idx):
+    code_list = code.split("\"")
+    otter_word = code_list[1]
+    otter_word_list = otter_word.split('_')
+    otter_word = '_'.join(otter_word_list[:-1])
+    with open('./scene_object.json', 'r') as f:
+        data = json.load(f)
+    keys = sorted(list(data.keys()))
+    sim_word = data[keys[idx]]
+    otter_word = [otter_word]
+    match_word = match(otter_word, sim_word)
+    code[1] = "\"" + match_word[0] + "\""
+    code_str = ''.join(code_list)
+    with open('./action.py', 'w') as f:
+        f.write(code_str)
     
     
 
 if __name__ == "__main__":
-    otter_word = ["meat_0"]
-    sim_word = ["electrical_fridge_abcd_0", "bacon_150", "fridge_abcd_0"]
-    match(otter_word, sim_word)
+    # otter_word = ["meat_abcd_150"]
+    # sim_word = ["electrical_fridge_abcd", "bacon", "fridge_abcd"]
+    # match(otter_word, sim_word)
+    # with open('./action.py', 'r') as f:
+    #     code = f.read()
+    #     change_code(code, 1)
+    all_data = {}
+    with open('./scene_object.json', 'r') as f:
+        data = json.load(f)
+    for i in data.keys():
+        words = data[i]
+        all_word = []
+        for word in words:
+            if word == 'robot0':
+                continue
+            word_list = word.split('_')
+            word_str = '_'.join(word_list[:-1])
+            all_word.append(word_str)
+        all_data[i] = all_word
+    with open('./scene_object.json', 'w') as f:
+        f.write(json.dumps(all_data))
